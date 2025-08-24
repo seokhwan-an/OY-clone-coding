d1bf15e8a4c193b3571057dd67803876b9d002e0
seokhwan-an <asok369258@naver.com>
2025-08-24 16:09:42 +0900

    test : product 수정 api 테스트 추가

--- src/test/kotlin/oystudy/clonecoding/product/controller/ProductControllerTest.kt
+++ src/test/kotlin/oystudy/clonecoding/product/controller/ProductControllerTest.kt
@@ -3,25 +3,42 @@ package oystudy.clonecoding.product.controller
 import io.restassured.RestAssured
 import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
-import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import oystudy.clonecoding.product.entity.Category
+import oystudy.clonecoding.product.entity.Name
+import oystudy.clonecoding.product.entity.Price
+import oystudy.clonecoding.product.entity.Product
+import oystudy.clonecoding.product.entity.repository.ProductRepository
 import oystudy.clonecoding.product.service.dto.CreateRequest
+import oystudy.clonecoding.product.service.dto.UpdateRequest
+import oystudy.clonecoding.product.service.dto.UpdateResponse
+import kotlin.test.Test
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductControllerTest(
     @LocalServerPort
-    private val port: Int
+    private val port: Int,
+
+    @Autowired
+    private val productRepository: ProductRepository
 ) {
 
     @BeforeEach
     fun setUp() {
         RestAssured.port = port
+    }
+
+    @AfterEach
+    fun deleteAll() {
+        productRepository.deleteAll()
     }
 
     @DisplayName("Product 생성")
@@ -39,6 +56,31 @@ class ProductControllerTest(
                 .post("/admin/products")
                 .then().log().all()
                 .statusCode(HttpStatus.CREATED.value())
+        }
+    }
+
+    @DisplayName("Product 수정")
+    @Nested
+    inner class Update {
+
+        @DisplayName("수정 성공")
+        @Test
+        fun success_update() {
+            val saveId = productRepository.save(Product(Name.from("아이템"), Price.from(200), Category.HAIR_CARE)).id!!
+            val request = UpdateRequest("아이템변경", 100, "스킨케어")
+
+            val updateResponse = given().log().all()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .put("/admin/products/$saveId")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract().`as`(UpdateResponse::class.java)
+
+            assertThat(updateResponse.id).isEqualTo(saveId)
+            assertThat(updateResponse.name).isEqualTo(request.name)
+            assertThat(updateResponse.price).isEqualTo(request.price)
+            assertThat(updateResponse.category).isEqualTo(request.category)
         }
     }
 }
--- src/test/kotlin/oystudy/clonecoding/product/service/ProductServiceTest.kt
+++ src/test/kotlin/oystudy/clonecoding/product/service/ProductServiceTest.kt
@@ -7,8 +7,14 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import oystudy.clonecoding.product.entity.Category
+import oystudy.clonecoding.product.entity.Name
+import oystudy.clonecoding.product.entity.Price
+import oystudy.clonecoding.product.entity.Product
 import oystudy.clonecoding.product.entity.repository.ProductRepository
 import oystudy.clonecoding.product.service.dto.CreateRequest
+import oystudy.clonecoding.product.service.dto.UpdateRequest
+import java.math.BigDecimal
 
 @SpringBootTest
 class ProductServiceTest(
@@ -78,6 +84,86 @@ class ProductServiceTest(
             val request = CreateRequest("아이템", 100, "스킨케어")
             val actual = productService.create(request)
             Assertions.assertThat(actual).isNotNull()
+        }
+    }
+
+    @DisplayName("Product 수정")
+    @Nested
+    inner class Update {
+
+        @DisplayName("수정 실패 - 존재하지 않는 상품")
+        @Test
+        fun fail_update_with_unknown() {
+            val wrongId = 0L
+            val request = UpdateRequest("아이템", 100, "스킨케어")
+            Assertions.assertThatThrownBy { productService.update(wrongId, request) }
+                .isInstanceOf(IllegalArgumentException::class.java)
+                .hasMessage("존재하지 않는 상품입니다.")
+        }
+
+        @DisplayName("수정 실패 - 상품 이름 공백 혹은 빈칸")
+        @Test
+        fun fail_update_name_with_empty_blank() {
+            val saveId = productRepository.save(Product(Name.from("아이템"), Price.from(200), Category.HAIR_CARE)).id!!
+            val requestWithBlankName = UpdateRequest("", 100, "스킨케어")
+            Assertions.assertThatThrownBy { productService.update(saveId, requestWithBlankName) }
+                .isInstanceOf(IllegalArgumentException::class.java)
+                .hasMessage("상품명은 비어 있을 수 없습니다.")
+        }
+
+        @DisplayName("수정 실패 - 상품 이름 길이 초과")
+        @Test
+        fun fail_update_name_with_length_exceed() {
+            val saveId = productRepository.save(Product(Name.from("아이템"), Price.from(200), Category.HAIR_CARE)).id!!
+            val requestWithExceedNameLength = UpdateRequest("아".repeat(51), 100, "스킨케어")
+            Assertions.assertThatThrownBy { productService.update(saveId, requestWithExceedNameLength) }
+                .isInstanceOf(IllegalArgumentException::class.java)
+                .hasMessage("상품명은 50자 이하여야 합니다.")
+        }
+
+        @DisplayName("수정 실패 - 상품 가격0 미만의 값")
+        @Test
+        fun fail_update_price_with_zero() {
+            val saveId = productRepository.save(Product(Name.from("아이템"), Price.from(200), Category.HAIR_CARE)).id!!
+            val requestWithPriceUnderZero = UpdateRequest("아이템", 0, "스킨케어")
+            Assertions.assertThatThrownBy { productService.update(saveId, requestWithPriceUnderZero) }
+                .isInstanceOf(IllegalArgumentException::class.java)
+                .hasMessage("상품 가격은 0원보다 커야 합니다.")
+        }
+
+        @DisplayName("수정 실패 - 상품 가격500,000 초과의 값")
+        @Test
+        fun fail_update_price_with_max() {
+            val saveId = productRepository.save(Product(Name.from("아이템"), Price.from(200), Category.HAIR_CARE)).id!!
+            val requestWithPriceOverMax = UpdateRequest("아이템", 500_001, "스킨케어")
+            Assertions.assertThatThrownBy { productService.update(saveId, requestWithPriceOverMax) }
+                .isInstanceOf(IllegalArgumentException::class.java)
+                .hasMessage("상품 가격은 500,000원보다 이하여야 합니다.")
+        }
+
+        @DisplayName("수정 실패 - 카테고리 존재하지 않는 값")
+        @Test
+        fun fail_update_category_with_unknown() {
+            val saveId = productRepository.save(Product(Name.from("아이템"), Price.from(200), Category.HAIR_CARE)).id!!
+            val requestWithWrongCategory = UpdateRequest("아이템", 100, "unknown")
+            Assertions.assertThatThrownBy { productService.update(saveId, requestWithWrongCategory) }
+                .isInstanceOf(IllegalArgumentException::class.java)
+                .hasMessage("존재하지 않는 카테고리 입니다.")
+        }
+
+        @DisplayName("수정 성공")
+        @Test
+        fun success_update() {
+            val saveId = productRepository.save(Product(Name.from("아이템"), Price.from(200), Category.HAIR_CARE)).id!!
+            val request = UpdateRequest("아이템변경", 100, "스킨케어")
+
+            productService.update(saveId, request)!!
+            val updatedProduct = productRepository.findById(saveId).get()
+
+            Assertions.assertThat(updatedProduct.id).isEqualTo(saveId)
+            Assertions.assertThat(updatedProduct.name.value).isEqualTo(request.name)
+            Assertions.assertThat(updatedProduct.price.value.toBigInteger()).isEqualTo(request.price)
+            Assertions.assertThat(updatedProduct.category.value).isEqualTo(request.category)
         }
     }
 }
